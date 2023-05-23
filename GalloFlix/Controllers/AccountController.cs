d4c23819b96e10972c042395f4bdbb4402daa1a0
Maria Clara Carvalho <mclara18062005@gmail.com>
2023-05-23 11:35:11 -0300

    Codificação do Login

--- GalloFlix/Controllers/AccountController.cs
+++ GalloFlix/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System.Net.Mail;
 using GalloFlix.DataTransferObjects;
 using GalloFlix.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -7,36 +8,79 @@ using Microsoft.AspNetCore.Mvc;
 namespace GalloFlix.Controllers;
 
 [Authorize(Roles = "Administrador")]
-    public class AccountController : Controller
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+    private readonly SignInManager<AppUser> _signInManager;
+    private readonly UserManager<AppUser> _userManager;
+
+    public AccountController(ILogger<AccountController> logger,
+         SignInManager<AppUser> signInManager,
+         UserManager<AppUser> userManager)
     {
-        private readonly ILogger<AccountController> _logger;
-        private readonlySignInManager<AppUser> _signInManager;
-        private readonly UserManager<AppUser> _userManager;
-
-        public AccountController(ILogger<AccountController> logger,
-             SignInManager<AppUser> signInManager,
-             UserManager<AppUser> userManager)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        [AllowAnonymous]
-        public IActionResult Login(string returnUrl)
-        {
-            LoginDto login = new();
-            login.ReturnUrl = returnUrl ?? Url.Content("~/");
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Login()
-        {
-            return View();
-        }
+        _logger = logger;
     }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult Login(string returnUrl)
+    {
+        LoginDto login = new();
+        login.ReturnUrl = returnUrl ?? Url.Content("~/");
+        return View();
+    }
+
+    [HttpPost]
+    [AllowAnonymous]
+    public async Task<IActionResult> Login(LoginDto login)
+    {
+        // Verificar o modelo e fazer o login
+        if (ModelState.IsValid) // Validação do lado do servidor
+        {
+            string userName = login.Email;
+            if (IsValidEmail(login.Email))
+            {
+                var user = await _userManager.FindByEmailAsync(login.Email);
+                if (user != null)
+                    userName = user.UserName;
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Password, login.RememberMe, lockoutOnFailure: true
+            );
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário {login.Email} acessou o sistema");
+                return LocalRedirect(login.ReturnUrl);
+            }
+            if (result.IsLockedOut)
+            {
+                _logger.LogWarning($"Usuário {login.Email} está bloqueado");
+                return RedirectToAction("Lockou");
+            }
+            ModelState.AddModelError("login", "Usuário e/ou Senha Inválidos!!!");
+        }
+        return View(login);
+    }
+
+
+
+    private bool IsValidEmail(string email)
+    {
+        try
+        {
+            MailAddress m = new(email);
+            return true;
+        }
+        catch (FormatException)
+        {
+            return false;
+        }
+
+    }
+}
