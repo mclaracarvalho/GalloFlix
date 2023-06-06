fd9d58ab7d4520c5b0b13a2f9ce9ebc4e8662e00
Maria Clara Carvalho <mclara18062005@gmail.com>
2023-06-06 11:40:08 -0300

    Criação do serviço de email e método de registro

--- GalloFlix/DataTransferObjects/RegisterDto.cs
+++ GalloFlix/DataTransferObjects/RegisterDto.cs
@@ -1,4 +1,4 @@
-using System.ComponentModel.DataAnnotation;
+using System.ComponentModel.DataAnnotations;
 
 namespace GalloFlix.DataTransferObjects;
     public class RegisterDto
@@ -11,20 +11,19 @@ namespace GalloFlix.DataTransferObjects;
 
         [Display(Name = "Email")]
         [Required(ErrorMessage = "Por favor, informe seu Email")]
-        [Required(ErrorMessage = "Por favor, informe um Email válido")]
+        [EmailAddress(ErrorMessage = "Por favor, informe um Email válido")]
         [StringLength(100, ErrorMessage = "O Email deve possuir no máximo 60 caracteres")]
         public string Email {get; set; }
         
         [DataType(DataType.Date)]
         [Display(Name = "Data de Nascimento")]
         [Required(ErrorMessage = "Por favor, informe sua Data de Nascimento")]
-        public DataTime DateOfBirth {get; set; }
+        public DateTime DateOfBirth {get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Senha de Acesso")]
         [Required(ErrorMessage = "Por favor, informe sua Senha")]
-        [StringLength(20, MinimumLenght = 6,
-        ErrorMessage = "A Senha deve possuir no mínimo 6 e máximo 20 caracteres")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "A Senha deve possuir no mínimo 6 e máximo 20 caracteres")]
         public string Password {get; set; }
 
         [DataType(DataType.Password)]
